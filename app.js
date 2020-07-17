a8f4566ba63ce7ff6160bd5068ae0616928dd03b
Fabio <fabio.sangregorio@gmail.com>
2020-07-17 20:54:46 +0200

    Solves #3, #4

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 'use strict';
 
+/** ========== TEMPLATES ========== */
+const dupIcon = `
+<div>
+  <div id="duplicate-event" class="uArJ5e Y5FYJe cjq2Db d29e1c"
+    jslog="74327; 2:[&quot;43aadic948fhep9u1afopevksk&quot;,&quot;acd52hqnogl5bokq04klqkdqfg@group.calendar.google.com&quot;,false,null,0,0,null,null,0,false,[1,2,null,[&quot;&quot;,2]],false]; track:JIbuQc"
+    jscontroller="VXdfxd"
+    jsaction="mouseenter:tfO1Yc; mouseleave:JywGue;touchstart:p6p2H; focus:AHmuwe; blur:O22p3e;"
+    jsshadow="" jsname="VkLyEc" 
+    aria-label="Duplicate event" aria-disabled="false" tabindex="0"
+    data-tooltip="Duplicate event" data-tooltip-vertical-offset="-12" data-tooltip-horizontal-offset="0">
+    <div class="PDXc1b MbhUzd"></div>
+    <span  class="XuQwKc">
+      <span class="GmuOkf">
+          <svg height="20" viewBox="0 0 24 24" width="20" focusable="false" class=" NMm5M"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4H8c-1.1 0-1.99.9-1.99 2L6 21c0 1.1.89 2 1.99 2H19c1.1 0 2-.9 2-2V11l-6-6zM8 21V7h6v5h5v9H8z"/></svg>
+      </span>
+    </span>
+  </div>
+</div>
+`;
+
+// Hide event menu when extension is active
+const css = `
+.gcqd-active .JPdR6b.Q3pIde.qjTEB {
+  display: none;
+}
+`;
+/** ======================================== */
+
+
 /** ========== CONSTANTS ========== */
 let intervalInjectIcon;
 let intervalDuplicateEvent;
@@ -12,6 +41,10 @@ let currentDate = "";
 
 /** ========== MAIN FUNCTION ========== */
 function app() {
+  var style = document.createElement('style');
+  document.head.appendChild(style);
+  style.type = 'text/css';
+  style.appendChild(document.createTextNode(css));
   /**
    * Inject the duplicate icon on a Google Calendar event click 
    */
@@ -32,26 +65,30 @@ function app() {
 };
 
 function duplicateEvent() {
+  document.body.classList.add('gcqd-active');
+  clearInterval(intervalDuplicateEvent);
   intervalDuplicateEvent = setInterval(function () {
-    /** Trigger click on the duplicate button in the options list */
-    var optionsButton = document.querySelector('div[aria-label=Options]');
-    var duplicateButton = document.querySelector('span[aria-label=Duplicate]');
-
-    if (optionsButton != null && duplicateButton != null) {
+    var optionsButton = document.querySelector('.pPTZAe .K2mXPb');
+    var duplicateButton = document.querySelector('.qjTEB [jsname="lbYRR"]');
+    if (optionsButton != null && duplicateButton == null) {
+      // Trigger click on the duplicate button in the options list
+      simulateClick(optionsButton);
+    } else if (duplicateButton != null) {
       // Then click the duplicate button
-      currentDate = getCurrentDate();
-      clearInterval(intervalDuplicateEvent);
-      simulateClick(duplicateButton);
+      currentDate = document.querySelector('.folmac').getAttribute('data-date');
+      simulateClick(duplicateButton.parentNode);
       saveEvent();
     }
   }, DELAY);
 }
 
 function saveEvent() {
+  clearInterval(intervalSaveEvent);
   intervalSaveEvent = setInterval(function() {
     /** Trigger save button when edit modal has opened */
-    var saveButton = document.querySelector('div[aria-label=Save]');
+    var saveButton = document.querySelector('[jsname="x8hlje"]');
     if (saveButton == null) return;
+    clearInterval(intervalDuplicateEvent);
     clearInterval(intervalSaveEvent);
     saveButton.click();
 
@@ -60,35 +97,32 @@ function saveEvent() {
 }
 
 function goToCurrentDate() {
+  clearInterval(intervalGoToDay);
   intervalGoToDay = setInterval(function() {
     if(location.href.includes("duplicate")) return;
     clearInterval(intervalGoToDay);
+
+    var d = new Date();
+    var date = padDate(d.getFullYear(), (d.getMonth() + 1), d.getDate());
+    console.log(currentDate, date);
+    if(currentDate !== date) {
+      // Doing this when duplicating an event on today date results in a change of view
+      console.log("true");
+      const miniDay = document.querySelector(".W0m3G");
+      const miniWeek = miniDay.parentNode;
+      const clonedDay = miniDay.cloneNode();
+      clonedDay.setAttribute("data-date", currentDate);
+      miniWeek.append(clonedDay);
+      clonedDay.click();
+      clonedDay.remove();
+    }
     
-    const miniDay = document.querySelector(".W0m3G");
-    const miniWeek = miniDay.parentNode;
-    const clonedDay = miniDay.cloneNode();
-    clonedDay.setAttribute("data-date", currentDate);
-    miniWeek.append(clonedDay); // TODO: try triggering click without appending
-    clonedDay.click();
-    clonedDay.remove();
+    document.body.classList.remove('gcqd-active');
   }, DELAY);
 }
 /** ======================================== */
 
 /** ========== UTILITY FUNCTIONS ========== */
-function getCurrentDate() {
-  let date;
-  let splittedUrl = location.href.split('/');
-  let len = splittedUrl.length;
-  if (isNaN(splittedUrl[len - 1]) || isNaN(splittedUrl[len - 2]) || isNaN(splittedUrl[len - 3])) {
-    const today = new Date();
-    date = padDate(today.getFullYear(), today.getMonth() + 1, today.getDay());
-  } else {
-    date = padDate(splittedUrl[len - 3], splittedUrl[len - 2], splittedUrl[len -1]);
-  }
-  return date;
-}
-
 function padDate(year, month, day) {
   return year + String(month).padStart(2, "0") + String(day).padStart(2, "0");
 }
@@ -109,6 +143,7 @@ function htmlToElement(html) {
 }
 
 function simulateClick(element) {
+  element.click();
   element.dispatchEvent(new MouseEvent("mousedown", { bubbles: true, cancelable: true, view: window }));
   element.dispatchEvent(new MouseEvent("mouseup", { bubbles: true, cancelable: true, view: window }))
 }
@@ -125,22 +160,3 @@ if (document.readyState === "complete" ||
 }
 /** ======================================== */
 
-/** ========== TEMPLATES ========== */
-const dupIcon = `
-<div>
-  <div id="duplicate-event" class="uArJ5e Y5FYJe cjq2Db d29e1c"
-    jslog="74327; 2:[&quot;43aadic948fhep9u1afopevksk&quot;,&quot;acd52hqnogl5bokq04klqkdqfg@group.calendar.google.com&quot;,false,null,0,0,null,null,0,false,[1,2,null,[&quot;&quot;,2]],false]; track:JIbuQc"
-    jscontroller="VXdfxd"
-    jsaction="click:cOuCgd; mousedown:UX7yZ; mouseup:lbsD7e; mouseenter:tfO1Yc; mouseleave:JywGue;touchstart:p6p2H; touchmove:FwuNnf; touchend:yfqBxc(preventMouseEvents=true|preventDefault=true); touchcancel:JMtRjd;focus:AHmuwe; blur:O22p3e; contextmenu:mg9Pef;"
-    jsshadow="" jsname="VkLyEc" 
-    aria-label="Duplicate event" aria-disabled="false" tabindex="0"
-    data-tooltip="Duplicate event" data-tooltip-vertical-offset="-12" data-tooltip-horizontal-offset="0">
-    <div class="PDXc1b MbhUzd"></div>
-    <span jsslot="" class="XuQwKc">
-      <span class="GmuOkf">
-          <svg height="20" viewBox="0 0 24 24" width="20" focusable="false" class=" NMm5M"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4H8c-1.1 0-1.99.9-1.99 2L6 21c0 1.1.89 2 1.99 2H19c1.1 0 2-.9 2-2V11l-6-6zM8 21V7h6v5h5v9H8z"/></svg>
-      </span>
-    </span>
-  </div>
-</div>
-`;
